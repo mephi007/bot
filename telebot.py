e6e3c3cc09b00755dc57ed104248b804b8d4fc85
mephi007 <roysumit949@gmail.com>
2018-08-07 18:07:23 +0530

    successfully interfaced camera and ultrasonic sensor, getting outuput and image

--- telebot.py
+++ telebot.py
@@ -1,5 +1,5 @@
 import telegram
-import picamera 
+ 
 import time
 from telegram.ext import Updater
 from telegram.ext import CommandHandler
@@ -7,8 +7,9 @@ import RPi.GPIO as GPIO
 from picamera import PiCamera
 
 
-
-
+#camera = PiCamera()
+#camera.rotation = 180
+#camera.resolution = (80, 60) 
 
 GPIO.setmode(GPIO.BCM)
 trig = 4
@@ -35,16 +36,19 @@ def sensor(trig , echo):
 	distance=duration*17150
 	distance=round(distance,2)
 	return distance	
+dist = sensor(trig , echo)
+#def notification():
+		
+#
+#def camera():
+#	camera = Picamera()
+#	camera.start_preview()
+#	sleep(0.1)
+#	camera.capture('/home/pi/Desktop/image.jpg')
+#	camera.stop_preview()
 
-def camera():
-	camera = Picamera()
-	camera.start_preview()
-	sleep(0.1)
-	camera.capture('/home/pi/Desktop/image.jpg')
-	camera.stop_preview()
 
-
-token = '694896074:AAFfXqgRXygr6ZUJ02mwIKfGeTlEKuxHg0A'
+token = ' token api key'
 updater = Updater(token = token)
 bot = telegram.Bot(token)
 intro = { ' hey' : 'hello' , 
@@ -60,29 +64,39 @@ updates = []
 while not updates:
 	updates=bot.getUpdates()
 	
-text = updates[-1].message.text
-print(text)
+#text = updates[-1].message.text
+#print(text)
 
-chat_id = updates[-1].message.chat.id
-print(chat_id)
+#chat_id = updates[-1].message.chat.id
+#print(chat_id)
 
 def start(bot , update):
-	bot.send_message(chat_id = chat_id , text="I'm a bot, send '/help' to get instruction")     
+	bot.send_message(chat_id = updates[-1].message.chat.id , text="I'm a bot, send '/help' to get instruction")     
   
 def help(bot , update):
-	bot.send_message(chat_id = chat_id , text=" I am able to send to you an alert if obstacle is nearer than 10 cm Send '/status' to know the sensor value Send '/image' to get image	")
+	bot.send_message(chat_id = updates[-1].message.chat.id , text=" I am able to send to you an alert if obstacle is nearer than 10 cm Send '/status' to know the sensor value Send '/image' to get image	")
 	
 def status(bot , update):
 	dist = sensor(trig , echo)
 	if (dist < 10): 
-		bot.send_message(chat_id = chat_id , text=" DANGER") 
-		camera()
-		bot.sendPhoto(chat_id = chat_id , tetx = 'image.jpg')
+		bot.send_message(chat_id = updates[-1].message.chat.id , text=" DANGER") 
+#		camera.start_preview()
+		
+#		sleep(1)
+		camera.capture('/home/pi/Desktop/bot/bot/image.jpg')
+#		camera.stop_preview()
+		bot.send_photo(chat_id = updates[-1].message.chat.id , photo =open ('image.jpg', 'rb'))
+		
 	else:
-		bot.send_message(chat_id = chat_id , text=" all ok")
+		bot.send_message(chat_id = updates[-1].message.chat.id , text=" all ok")
 		
 def image (bot , update):
-	bot.send_message(chat_id = chat_id , text=" ") 
+	bot.send_message(chat_id = updates[-1].message.chat.id , text=" image")
+	#camera.start_preview()
+	#sleep(1)
+	camera.capture('/home/pi/Desktop/bot/bot/image.jpg')
+	#camera.stop_preview()
+	bot.send_photo(chat_id = updates[-1].message.chat.id , photo =open ('image.jpg', 'rb'))
 #	time.sleep(5)
 
 start_handler = CommandHandler('start' , start)
@@ -97,7 +111,5 @@ dispatcher.add_handler(status_handler)
 image_handler = CommandHandler('image' , image)
 dispatcher.add_handler(image_handler)
 	
-
-	
-
 updater.start_polling()
+updater.idle()
